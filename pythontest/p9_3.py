87b82fafc3136680850a2938e030b1c9029793dc
15816040 <a5816040@gmail.com>
2018-06-22 13:41:00 +0900

    My Django Girls app, first commit

--- pythontest/p9_3.py
+++ pythontest/p9_3.py
@@ -3,17 +3,17 @@ from itertools import cycle
 import matplotlib.pyplot as plt
 from matplotlib.sankey import Sankey
 
-links_per_side = 6
+links_per_side = 8
 
 
 def side(sankey, n=1):
     """Generate a side chain."""
     prior = len(sankey.diagrams)
     for i in range(0, 2*n, 2):
-        sankey.add(flows=[1, -1], orientations=[-1, -1],
+        sankey.add(flows=[5, -5], orientations=[-5, -5],
                    patchlabel=str(prior + i),
                    prior=prior + i - 1, connect=(1, 0), alpha=0.5)
-        sankey.add(flows=[1, -1], orientations=[1, 1],
+        sankey.add(flows=[5, -5], orientations=[5, 5],
                    patchlabel=str(prior + i + 1),
                    prior=prior + i, connect=(1, 0), alpha=0.5)
 
@@ -21,7 +21,7 @@ def side(sankey, n=1):
 def corner(sankey):
     """Generate a corner link."""
     prior = len(sankey.diagrams)
-    sankey.add(flows=[1, -1], orientations=[0, 1],
+    sankey.add(flows=[5, -5], orientations=[0, 1],
                patchlabel=str(prior), facecolor='k',
                prior=prior - 1, connect=(1, 0), alpha=0.5)
 
